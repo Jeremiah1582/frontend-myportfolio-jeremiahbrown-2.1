961fb39e2240d68f4d6d00dd6f2ea6d26ff7f862
jeremiah.1582@googlemail.com <jeremiah.1582@googlemail.com>
2022-11-21 13:12:33 +0100

    sales and marketing service update 2

--- src/Components/Sections/ServicesSection.js
+++ src/Components/Sections/ServicesSection.js
@@ -19,7 +19,8 @@ function ServicesSection() {
         <div className="innerTextBox">
           <h4>Services</h4>
           <p>
-          Looking for someone to build your idea but don't know where to start? Don't worry you're in the right place. Whether you need a UX/UI Designer, Someone to migrate your data or maybe you just need someone to point you in the right direction. Whatever it is, I am here to help. Together with my professional skills, my library of life experience influences how I work and allows me to look at your challenges from a new perspective. 
+          Looking for someone to build your vision but don't know where to start? <br /> Don't worry you're in the right place. 
+          <br /> Whether you need a UX/UI Designer, Someone to migrate your data or maybe you just need someone to point you in the right direction. Whatever it is, I am here to help. My professional skills and my library of life experience influences every part of my work, allowing me to look at your challenges from new perspectives. 
           </p>
         </div>
       </div>

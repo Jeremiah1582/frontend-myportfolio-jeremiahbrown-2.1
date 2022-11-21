8deb519c4c2e8d230409f9c79a399ab57abce2f6
jeremiah.1582@googlemail.com <jeremiah.1582@googlemail.com>
2022-11-21 13:13:48 +0100

    spell check correction

--- src/Components/Sections/ServicesSection.js
+++ src/Components/Sections/ServicesSection.js
@@ -20,7 +20,7 @@ function ServicesSection() {
           <h4>Services</h4>
           <p>
           Looking for someone to build your vision but don't know where to start? <br /> Don't worry you're in the right place. 
-          <br /> Whether you need a UX/UI Designer, Someone to migrate your data or maybe you just need someone to point you in the right direction. Whatever it is, I am here to help. My professional skills and my library of life experience influences every part of my work, allowing me to look at your challenges from new perspectives. 
+          <br /> Whether you need a UX/UI Designer, Someone to migrate your data or maybe you just need someone to point you in the right direction. Whatever it is, I am here to help. My professional skills and my library of life experience influence every part of my work, allowing me to look at your challenges from new perspectives. 
           </p>
         </div>
       </div>

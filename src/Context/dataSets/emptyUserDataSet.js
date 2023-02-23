094bdeebd741ff96f4b370645b38b888f85237f6
jeremiah.1582@googlemail.com <jeremiah.1582@googlemail.com>
2023-02-23 11:05:20 +0100

    removed Facebook Social link

--- src/Context/dataSets/emptyUserDataSet.js
+++ src/Context/dataSets/emptyUserDataSet.js
@@ -14,11 +14,11 @@ export const defaultUserState = {
       img: "https://cdn1.iconfinder.com/data/icons/logotypes/32/github-256.png",
       link: "https://github.com/Jeremiah1582",
     },
-    {
-      name: "facebook",
-      img: "https://cdn3.iconfinder.com/data/icons/free-social-icons/67/facebook_circle_color-256.png",
-      link: "https://www.facebook.com/jeremiahglobetrotterbrown.1582",
-    },
+    // {
+    //   name: "facebook",
+    //   img: "https://cdn3.iconfinder.com/data/icons/free-social-icons/67/facebook_circle_color-256.png",
+    //   link: "https://www.facebook.com/jeremiahglobetrotterbrown.1582",
+    // },
     // {
     //   name: "instagram",
     //   img: "https://cdn3.iconfinder.com/data/icons/2018-social-media-logotypes/1000/2018_social_media_popular_app_logo_instagram-256.png",

28fe3dfe7f5b0eb9f5fac4d7402fc2be49939ad3
jeremiah.1582@googlemail.com <jeremiah.1582@googlemail.com>
2023-03-09 17:54:58 +0100

    styling uppdates

--- src/Components/Sections/CertificatesSection.js
+++ src/Components/Sections/CertificatesSection.js
@@ -3,17 +3,25 @@ import Figure from 'react-bootstrap/Figure';
 import { certificates } from '../../Context/dataSets/certificates';
 
 function CertificatesSection() {
+  const handleVerification=(link)=>{
+    console.log('clicked');
+    window.location.href=link
+  }
   return (
     <div className='certificateSection  '>   
       {certificates.map(cert=>{
         return (
         <Figure >
+         
           <Figure.Image
             className='certImg'
             alt={cert.name}
             src={cert.image}
+            onClick={cert.verificationLink? ()=>handleVerification(cert.verificationLink):null}
+        
           /> 
           </Figure>
+        
         )
       })}</div>
    
